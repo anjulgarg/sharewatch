64ce61571ee95d3f4dc10707103bd64554519395
Anjul Garg <anjulgarg@live.com>
2019-03-29 11:43:40 +0530

    minimum node version requirement lowered to 6.11.0

--- README.md
+++ README.md
@@ -4,35 +4,75 @@
 
 Fetch Real-time Indian stock market data from NSE (National Stock Exchange) and BSE (Bombay Stock Exchange)
 
-### Installation
+## Features
 
-Sharewatch requires [Node.js](https://nodejs.org/) v8.10.0+ to run.
+- List all companies under NSE and BSE India along with their ISIN, Scrip Code etc.
+- Fetch Real-Time NSE and BSE indice data like NIFTY50 Index and BSE Index
+- Fetch Real-Time Quote (live price) for any Equity listed under NSE and BSE
+
+## Installation
+
+Sharewatch requires [Node.js](https://nodejs.org/) v6.11.0+ to run.
 ```sh
 $ npm install sharewatch
 ```
 
-### Example Usage
+## Example Usage
 
-Fetch the list of all Equity Securities listed under NSE India
+### Fetch the list of all Equity Securities listed under NSE India
+
 ```sh
 const NSE = require('sharewatch').NSE
 let result = await NSE.equityList()
 ```
 
-Similarly, you can fetch this list for BSE India
+OR if you want to use Promises
+
+```sh
+const NSE = require('sharewatch').NSE
+NSE.equityList()
+    .then((result) => {
+        // use `result`
+    })
+    .catch((err) => {
+        // handle error
+    })
+```
+
+### Similarly, you can fetch this list for BSE India
+
 ```sh
 const BSE = require('sharewatch').BSE
 let result = await BSE.equityList()
 ```
 
-Fetch real-time NSE Indice data
+### Fetch real-time NSE and BSE Indice data
+
 ```sh
 const NSE = require('sharewatch').NSE
 let result = await NSE.indices()
 ```
 
-Fetch real-time NSE Stock Quote
+```sh
+const BSE = require('sharewatch').BSE
+let result = await BSE.indices()
+```
+
+### Fetch real-time NSE and BSE Stock Quote
+
 ```sh
 const NSE = require('sharewatch').NSE
 let result = await NSE.quote('INFY')
+```
+
+```sh
+const BSE = require('sharewatch').BSE
+let result = await BSE.quote('INFY')
+```
+
+### Fetch real-time BSE Stock Quote with Peer comparison
+
+```sh
+const BSE = require('sharewatch').BSE
+let result = await BSE.quoteWithComparison('INFY')
 ```
--- test/services.js
+++ test/services.js
@@ -8,25 +8,31 @@ describe('NSE', function () {
   this.timeout(0)
 
   describe('#equityList()', function () {
-    it('should return a valid Array', async function () {
-      let result = await NSE.equityList()
-      expect(result).to.be.an('Array').with.lengthOf.above(100)
+    it('should return a valid Array', function () {
+      return NSE.equityList()
+        .then((result) => {
+          expect(result).to.be.an('Array').with.lengthOf.above(100)
+        })
     })
   })
 
   describe('#indices()', function () {
-    it('should return a valid JSON Object', async function () {
-      let result = await NSE.indices()
-      expect(result).to.be.a('Object')
-      expect(result).to.contain.keys(['mktOpen', 'data'])
+    it('should return a valid JSON Object', function () {
+      return NSE.indices()
+        .then((result) => {
+          expect(result).to.be.a('Object')
+          expect(result).to.contain.keys(['mktOpen', 'data'])
+        })
     })
   })
 
   describe('#quote()', function () {
-    it('should return a valid JSON Object', async function () {
-      let result = await NSE.quote(symbol='INFY')
-      expect(result).to.be.a('Object')
-      expect(result).to.contain.keys(['data'])
+    it('should return a valid JSON Object', function () {
+      return NSE.quote(symbol = 'INFY')
+        .then((result) => {
+          expect(result).to.be.a('Object')
+          expect(result).to.contain.keys(['data'])
+        })
     })
   })
 })
@@ -35,33 +41,41 @@ describe('BSE', function () {
   this.timeout(0)
 
   describe('#equityList()', function () {
-    it('should return a valid Array', async function () {
-      let result = await BSE.equityList()
-      expect(result).to.be.an('Array').with.lengthOf.above(100)
+    it('should return a valid Array', function () {
+      return BSE.equityList()
+        .then((result) => {
+          expect(result).to.be.an('Array').with.lengthOf.above(100)
+        })
     })
   })
 
   describe('#indices()', function () {
-    it('should return a valid JSON Object', async function () {
-      let result = await BSE.indices()
-      expect(result).to.be.a('Object')
-      expect(result).to.contain.keys(['ltp', 'chg'])
+    it('should return a valid JSON Object', function () {
+      return BSE.indices()
+        .then((result) => {
+          expect(result).to.be.a('Object')
+          expect(result).to.contain.keys(['ltp', 'chg'])
+        })
     })
   })
 
   describe('#quote()', function () {
-    it('should return a valid JSON Object', async function () {
-      let result = await BSE.quote(scripCode='500209')
-      expect(result).to.be.a('Object')
-      expect(result).to.contain.keys(['PrevClose'])
+    it('should return a valid JSON Object', function () {
+      return BSE.quote(scripCode = '500209')
+        .then((result) => {
+          expect(result).to.be.a('Object')
+          expect(result).to.contain.keys(['PrevClose'])
+        })
     })
   })
 
   describe('#quoteWithComparison()', function () {
-    it('should return a valid JSON Object', async function () {
-      let result = await BSE.quoteWithComparison(scripCode='500209')
-      expect(result).to.be.an('Object')
-      expect(result).to.contain.keys(['Table'])
+    it('should return a valid JSON Object', function () {
+      return BSE.quoteWithComparison(scripCode = '500209')
+        .then((result) => {
+          expect(result).to.be.an('Object')
+          expect(result).to.contain.keys(['Table'])
+        })
     })
   })
 })
