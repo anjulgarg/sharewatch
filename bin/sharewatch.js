72bdefa2a708d2f9c68a53adb43ed33d4cc178cc
Anjul Garg <anjulgarg@live.com>
2019-04-03 13:10:02 +0530

    adding bhavcopy and sector stats API

---
+++ bin/sharewatch.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/*!
+ * sharewatch
+ * Copyright(c) 2019 Anjul Garg <anjulgarg@live.com>
+ * MIT Licensed
+ */
+
+'use strict'
+
+/**
+ * Module dependencies
+ * @private
+ */
+const program = require('commander')
+const packageFile = require('../package.json')
+
+program
+  .version(packageFile['version'])
+  .option('-p, --platform <p>', 'Valid choices are `bse` and `nse`', /^(bse|nse)$/i)
+  .option('-o, --output', 'Output file path')
+
+program
+  .command('equity-list')
+
+program
+  .command('indices')
+  // .option('-p, --platform <p>', 'Valid choices are `bse` and `nse`', /^(bse|nse)$/i)
+
+program
+  .command('nse-quote') 
+  .option('-isin, --isin', 'ISIN of the stock')
+
+program
+  .command('bse-quote') 
+  .option('-sc, --scrip-code', 'Scrip Code of the Stock')
+
+program
+  .parse(process.argv);
--- test/services.js
+++ test/services.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const assert = require('chai').assert
 const expect = require('chai').expect
 const NSE = require('../index').NSE
@@ -35,47 +36,68 @@ describe('NSE', function () {
         })
     })
   })
-})
 
-describe('BSE', function () {
-  this.timeout(0)
-
-  describe('#equityList()', function () {
-    it('should return a valid Array', function () {
-      return BSE.equityList()
-        .then((result) => {
-          expect(result).to.be.an('Array').with.lengthOf.above(100)
-        })
-    })
-  })
-
-  describe('#indices()', function () {
+  describe('#sectorDetails()', function () {
     it('should return a valid JSON Object', function () {
-      return BSE.indices()
+      return NSE.sectorDetails(symbol = 'INFY')
         .then((result) => {
           expect(result).to.be.a('Object')
-          expect(result).to.contain.keys(['ltp', 'chg'])
+          expect(result).to.contain.keys(['sector'])
         })
     })
   })
 
-  describe('#quote()', function () {
+  describe('#bhavcopy()', function () {
     it('should return a valid JSON Object', function () {
-      return BSE.quote(scripCode = '500209')
+      let path = './bhavcopy2018-02-02.zip'
+      return NSE.bhavcopy(date = '2018-02-02', path)
         .then((result) => {
-          expect(result).to.be.a('Object')
-          expect(result).to.contain.keys(['PrevClose'])
-        })
-    })
-  })
-
-  describe('#quoteWithComparison()', function () {
-    it('should return a valid JSON Object', function () {
-      return BSE.quoteWithComparison(scripCode = '500209')
-        .then((result) => {
-          expect(result).to.be.an('Object')
-          expect(result).to.contain.keys(['Table'])
+          expect(fs.existsSync(path)).to.be.true
+          fs.unlinkSync(path)
         })
     })
   })
 })
+
+// describe('BSE', function () {
+//   this.timeout(0)
+
+//   describe('#equityList()', function () {
+//     it('should return a valid Array', function () {
+//       return BSE.equityList()
+//         .then((result) => {
+//           expect(result).to.be.an('Array').with.lengthOf.above(100)
+//         })
+//     })
+//   })
+
+//   describe('#indices()', function () {
+//     it('should return a valid JSON Object', function () {
+//       return BSE.indices()
+//         .then((result) => {
+//           expect(result).to.be.a('Object')
+//           expect(result).to.contain.keys(['ltp', 'chg'])
+//         })
+//     })
+//   })
+
+//   describe('#quote()', function () {
+//     it('should return a valid JSON Object', function () {
+//       return BSE.quote(scripCode = '500209')
+//         .then((result) => {
+//           expect(result).to.be.a('Object')
+//           expect(result).to.contain.keys(['PrevClose'])
+//         })
+//     })
+//   })
+
+//   describe('#quoteWithComparison()', function () {
+//     it('should return a valid JSON Object', function () {
+//       return BSE.quoteWithComparison(scripCode = '500209')
+//         .then((result) => {
+//           expect(result).to.be.an('Object')
+//           expect(result).to.contain.keys(['Table'])
+//         })
+//     })
+//   })
+// })
