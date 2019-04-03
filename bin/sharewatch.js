294a729cb8bc792afc2a4293959ddd591fa83fde
Anjul Garg <anjulgarg@live.com>
2019-04-03 15:52:19 +0530

    lint

--- bin/sharewatch.js
+++ bin/sharewatch.js
@@ -12,28 +12,28 @@
  * Module dependencies
  * @private
  */
-const program = require('commander')
-const packageFile = require('../package.json')
+// const program = require('commander')
+// const packageFile = require('../package.json')
 
-program
-  .version(packageFile['version'])
-  .option('-p, --platform <p>', 'Valid choices are `bse` and `nse`', /^(bse|nse)$/i)
-  .option('-o, --output', 'Output file path')
+// program
+//   .version(packageFile['version'])
+//   .option('-p, --platform <p>', 'Valid choices are `bse` and `nse`', /^(bse|nse)$/i)
+//   .option('-o, --output', 'Output file path')
 
-program
-  .command('equity-list')
+// program
+//   .command('equity-list')
 
-program
-  .command('indices')
-  // .option('-p, --platform <p>', 'Valid choices are `bse` and `nse`', /^(bse|nse)$/i)
+// program
+//   .command('indices')
+//   // .option('-p, --platform <p>', 'Valid choices are `bse` and `nse`', /^(bse|nse)$/i)
 
-program
-  .command('nse-quote') 
-  .option('-isin, --isin', 'ISIN of the stock')
+// program
+//   .command('nse-quote') 
+//   .option('-isin, --isin', 'ISIN of the stock')
 
-program
-  .command('bse-quote') 
-  .option('-sc, --scrip-code', 'Scrip Code of the Stock')
+// program
+//   .command('bse-quote') 
+//   .option('-sc, --scrip-code', 'Scrip Code of the Stock')
 
-program
-  .parse(process.argv);
+// program
+//   .parse(process.argv);
